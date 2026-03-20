355d5f923467c3e7f57a2065b60d9a43e27f2312
Alok Yadav <alokkumaryadav.cse@gmail.com>
2026-03-20 18:08:36 +0530

    feat: refactor to make scalable and add support for margin and padding

--- main.js
+++ main.js
@@ -1,18 +1,57 @@
-const myStyles = {
-    "m-4": "margin: 4px",
-    "bg-red": "background-color: red",
-    "pt-8": "padding-top: 32px"
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    document.body.querySelectorAll("[class^='chai-'], [class*=' chai-']").forEach(processElement)
 }
 
-const allElements = document.body.querySelectorAll("*")
-allElements.forEach(element =>{
-    const m = "margin"
-    // element.style[m] = "4px"
-    element.classList
-        .forEach(cls => {
-            console.log(myStyles[cls]);
-            const property = myStyles[cls].split(":")[0];
-            const value = myStyles[cls].split(":")[1];
-            element.style[property] = value;
-        })
-})
+function processElement(el) {
+    const styles = {
+        "backgroundColor": "pink"
+    };
+
+    for(const className of el.classList){
+        if(!className.startsWith("chai-")) continue;
+
+        const style = parseClass(className);
+        Object.assign(styles, style);
+    }
+
+    Object.assign(el.style, styles);
+}
+
+const utilities = {
+    m: marginHandler,
+    p: paddingHandler,
+}
+
+const spacingScale = {
+    0: "0px",
+    1: "4px",
+    2: "8px",
+    3: "12px",
+    4: "16px",
+    5: "20px",
+};
+
+function marginHandler (value){
+    return {
+        "margin": spacingScale[value],
+    }
+}
+
+function paddingHandler (value){
+    return {
+        "padding": spacingScale[value],
+    }
+}
+
+
+
+function parseClass(className) {
+    const parts = className.split("-");
+    const prefix = parts[1];
+
+    const handler = utilities[prefix];
+    const style = handler(parts[2])
+    return style;
+}
