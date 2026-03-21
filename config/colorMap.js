3688e92e102418c2cb18351774300b3e3e32cc08
Alok Yadav <alokkumaryadav.cse@gmail.com>
2026-03-22 03:59:43 +0530

    feat: add some nice colors

--- config/colorMap.js
+++ config/colorMap.js
@@ -1,8 +1,20 @@
 export const colorMap = {
-    red: "#ff0000",
-    green: "#00ff00",
-    blue: "#0000ff",
+    red: "#ef4444",
+    green: "#22c55e",
+    blue: "#3b82f6",
     black: "#000000",
     white: "#ffffff",
-    gray: "#808080",
+
+    gray: "#9ca3af",
+    slate: "#64748b",
+    neutral: "#404040",
+    zinc: "#18181b",
+
+    indigo: "#6366f1",
+    violet: "#8b5cf6",
+    sky: "#0ea5e9",
+    emerald: "#10b981",
+    rose: "#f43f5e",
+    amber: "#f59e0b",
+    orange: "#f97316",
 }
